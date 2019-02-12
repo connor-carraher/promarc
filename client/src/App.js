0c04cfe16199119da74aca4f188a120b95b06a4a
Connor Carraher <connor@carraher.com>
2019-02-12 00:11:30 -0800

    Session attempt

--- client/src/App.js
+++ client/src/App.js
@@ -15,26 +15,42 @@ import axios from "axios";
 import { Button } from "reactstrap";
 import Navbar from "./Navbar";
 import Login from "./Login";
+import Cookies from "js-cookie";
 
 class App extends Component {
   state = {
-    data: []
+    data: [],
+    loggedIn: false
   };
 
   render() {
+    const authprops = {
+      loggedIn: this.state.loggedIn
+    };
+
+    console.log(document.cookie);
+    if (Cookies.get("session") != null) {
+      this.state.loggedIn = true;
+    }
     return (
-      <div>
-        <Navbar />
+      <React.Fragment>
         <Router>
-          <Switch>
-            <Route path="/test" component={Test} />
-            <Route path="/createpost" component={CreatePost} />
-            <Route path="/viewpost" component={PostCard} />
-            <Route path="/post/:id" component={Post} />
-            <Route path="/login" component={Login} />
-          </Switch>
+          {this.state.loggedIn ? (
+            <React.Fragment>
+              <Navbar />
+              <Switch>
+                <Route path="/" />
+                <Route path="/test" component={Test} />
+                <Route path="/createpost" component={CreatePost} />
+                <Route path="/viewpost" component={PostCard} />
+                <Route path="/post/:id" component={Post} />
+              </Switch>
+            </React.Fragment>
+          ) : (
+            <Route path="/" component={Login} />
+          )}
         </Router>
-      </div>
+      </React.Fragment>
     );
   }
 }
