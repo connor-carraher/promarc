e351de9c316df4936a84b8dce5e47fbf40881905
Connor Carraher <connor@carraher.com>
2019-02-13 23:46:26 -0800

    Import errors

--- client/src/PostCard.js
+++ client/src/PostCard.js
@@ -1,6 +1,5 @@
 // /client/App.js
 import React, { Component } from "react";
-import {Container} from "reactstrap";
 
 class PostCard extends Component {
   state = {
@@ -30,25 +29,63 @@ class PostCard extends Component {
     const { data } = this.state;
     return (
       <div>
-        <div style={{borderRight:"2px solid lightgray",float:"left", width:"75%"}}>
-        <ul>
-          {data.length <= 0
-            ? "NO DB ENTRIES YET"
-            : data.map(dat => (
-                <li style={{ listStyleType: "none", margin: "10px", paddingTop: "5px" }} key={dat._id}>
-                  {/*<span style={{ color: "gray" }}> id: </span> {dat._id} <br />
+        <div
+          style={{
+            borderRight: "2px solid lightgray",
+            float: "left",
+            width: "75%"
+          }}
+        >
+          <ul>
+            {data.length <= 0
+              ? "NO DB ENTRIES YET"
+              : data.map(dat => (
+                  <li
+                    style={{
+                      listStyleType: "none",
+                      margin: "10px",
+                      paddingTop: "5px"
+                    }}
+                    key={dat._id}
+                  >
+                    {/*<span style={{ color: "gray" }}> id: </span> {dat._id} <br />
                   <span style={{ color: "gray" }}> Title: </span>{" "}*/}
-                  <a style={{fontSize:"24pt"}}href={"/post/" + dat._id}>{dat.title}</a> <br />
-                  <span style={{ fontWeight:"bold", fontSize: "16pt", color: "black" }}> Skills: </span> <span style={{fontSize:"16pt"}}>{dat.skills}</span> <br />
-                  <span style={{ fontWeight:"bold", fontSize: "12pt", color: "gray" }}> Description: </span> <span style={{fontSize:"12pt"}}>{dat.description}</span> <br />
-                  <hr />
-                </li>
-              ))}
-        </ul>
+                    <a style={{ fontSize: "24pt" }} href={"/post/" + dat._id}>
+                      {dat.title}
+                    </a>{" "}
+                    <br />
+                    <span
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: "16pt",
+                        color: "black"
+                      }}
+                    >
+                      {" "}
+                      Skills:{" "}
+                    </span>{" "}
+                    <span style={{ fontSize: "16pt" }}>{dat.skills}</span>{" "}
+                    <br />
+                    <span
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: "12pt",
+                        color: "gray"
+                      }}
+                    >
+                      {" "}
+                      Description:{" "}
+                    </span>{" "}
+                    <span style={{ fontSize: "12pt" }}>
+                      {dat.description}
+                    </span>{" "}
+                    <br />
+                    <hr />
+                  </li>
+                ))}
+          </ul>
         </div>
-        <div style={{float:"left", width:"25%"}}>
-          
-        </div>
+        <div style={{ float: "left", width: "25%" }} />
       </div>
     );
   }
