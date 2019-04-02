01fa37867f6e42a2a67b75de32492153a24b8739
Connor Carraher <connor@carraher.com>
2019-04-02 14:23:49 -0700

    wip

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ passport.use(
       callbackURL:
         process.env.NODE_ENV === "development"
           ? "http://localhost:3001/api/auth/google/callback"
-          : "http://promarc-prod.com/api/auth/google/callback"
+          : "http://connorcarraeher.com/api/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
       /*if (profile._json["domain"] != "scu.edu") {
